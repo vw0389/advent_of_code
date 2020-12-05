3ed56be9627c4e31e0786ec5504733ff05211481
vw8562 <>
2020-12-04 18:41:25 -0600

    day 2 part 2

--- 2020/2/2.py
+++ 2020/2/2.py
@@ -1,14 +1,9 @@
 #!/bin/python3
-# Each line gives the password policy and then the password.
-# The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid.
-#  For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
-# In the above example, 2 passwords are valid. The middle password, cdefg, is not;
-# it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
-
-# How many passwords are valid according to their policies?
+import os
+filename = os.path.join(os.path.dirname(__file__), '2_input.txt')
 passwords = []
-answer = 0
-with open('2_input.txt',mode='r') as f:
+answer1, answer2 = 0, 0
+with open(filename,mode='r') as f:
    passwords = [line.rstrip() for line in f]
 for i in passwords:
     things = i.split(' ')
@@ -18,7 +13,17 @@ for i in passwords:
     min = int(things[0])
     max = int(things[1])
 
-    number = password.count(letter)
-    if number <=  max and number >= min:
-        answer = answer + 1
-print(str(answer))
+    count = password.count(letter)
+    if count <=  max and count >= min:
+        answer1 = answer1 + 1
+    minB, maxB = False, False
+    if password[min-1] == letter:
+        minB = True 
+    if password[max-1] == letter:
+        maxB = True
+    if minB == maxB:
+        continue
+    else:
+        answer2 = answer2 + 1
+print(str(answer1))
+print(str(answer2))
