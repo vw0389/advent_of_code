6ee9cbde6fa96f76091b4e2432e0a375996c1aba
vw8562 <>
2020-12-04 18:10:04 -0600

    day 1 part 2

--- 2020/1/1.py
+++ 2020/1/1.py
@@ -1,14 +1,26 @@
 #!/bin/pyton3
+import os
+filename = os.path.join(os.path.dirname(__file__), '1_input.txt')
+
 nums = []
-answer = None
-with open('1_input.txt',mode='r') as f:
+answer1 = answer2 = None
+with open(filename,mode='r') as f:
    nums = [int(line.rstrip()) for line in f] 
 
 for i in nums:
     for j in nums:
         if i + j == 2020:
-            answer = i * j
+            answer1 = i * j
             break
-    if answer != None:
+    if answer1 != None:
         break
-print(str(answer))
+for i in nums:
+    for j in nums:
+        for k in nums:
+            if i + j + k == 2020:
+                answer2 = i * j * k
+                break
+    if answer2 != None: 
+        break
+print("Part 1: " + str(answer1))
+print("Part 2: " + str(answer2))
